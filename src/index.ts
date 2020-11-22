85e8fba09e62b8569336049854e696cffcb0695f
Rafael Pedicini <rafael@rafgraph.dev>
2020-11-22 10:36:30 -0500

    Add random color generator and tests

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-export const test = 'hello world';
+const randomNumber0to255 = () => Math.floor(256 * Math.random());
+
+interface RandomRGBColorGenerator {
+  (): [number, number, number];
+}
+
+export const randomRGBColorGenerator: RandomRGBColorGenerator = () => [
+  randomNumber0to255(),
+  randomNumber0to255(),
+  randomNumber0to255(),
+];
