62fc70f82b2fa3a08974d8a10337373ede7d3d0f
Rafael Pedicini <rafael@rafgraph.dev>
2020-11-22 13:34:45 -0500

    Add some dev mode code

--- src/randomRGBColorGenerator.ts
+++ src/randomRGBColorGenerator.ts
@@ -4,8 +4,14 @@ interface RandomRGBColorGenerator {
   (): [number, number, number];
 }
 
-export const randomRGBColorGenerator: RandomRGBColorGenerator = () => [
-  randomNumber0to255(),
-  randomNumber0to255(),
-  randomNumber0to255(),
-];
+export const randomRGBColorGenerator: RandomRGBColorGenerator = () => {
+  const red = randomNumber0to255();
+  const green = randomNumber0to255();
+  const blue = randomNumber0to255();
+
+  if (process.env.NODE_ENV !== 'production') {
+    console.log(`Color generated: red ${red}, green ${green}, blue ${blue}`);
+  }
+
+  return [red, green, blue];
+};
